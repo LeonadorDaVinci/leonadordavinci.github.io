64bf52ad5769968936db4e81443bec3fd5910ece
LeonadorDaVinci <104429265+LeonadorDaVinci@users.noreply.github.com>
2023-01-20 16:07:20 +0800

    Add files via upload

---
+++ interactivemap/army.js
@@ -0,0 +1,199 @@
+function checkChunkArmy (object){
+    var ChunkCoordinate = object;
+
+    if (ChunkCoordinate == "A/8"){
+        document.getElementById("Army1").innerHTML="宏朝";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="250k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "A/9"){
+        document.getElementById("Army1").innerHTML="宏朝";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="250k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "A/10"){
+        document.getElementById("Army1").innerHTML="宏朝";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="250k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "B/10"){
+        document.getElementById("Army1").innerHTML="宏朝";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="250k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "C/4"){
+        document.getElementById("Army1").innerHTML="亚特兰蒂斯";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="84k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "C/8"){
+        document.getElementById("Army1").innerHTML="联众邦驻军";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="15k";
+        display("army1personel");
+    }
+    
+    
+    else if (ChunkCoordinate == "C/10"){
+        document.getElementById("Army1").innerHTML="宏朝";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="440k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "D/4"){
+        document.getElementById("Army1").innerHTML="亚特兰蒂斯";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "D/5"){
+        document.getElementById("Army1").innerHTML="亚特兰蒂斯";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "D/6"){
+        document.getElementById("Army1").innerHTML="亚特兰蒂斯";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "E/8"){
+        document.getElementById("Army1").innerHTML="美利坚社会主义合众国";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="160k";
+        display("army1personel");
+    }
+    
+    
+    else if (ChunkCoordinate == "F/8"){
+        document.getElementById("Army1").innerHTML="美利坚社会主义合众国";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="140k";
+        display("army1personel");
+    }
+    
+    
+    else if (ChunkCoordinate == "F/9"){
+        document.getElementById("Army1").innerHTML="阿科诺夫社会主义联邦";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="90k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "G/3"){
+        document.getElementById("Army1").innerHTML="羊村";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="50k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "G/4"){
+        document.getElementById("Army1").innerHTML="羊村";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="56k";
+        display("army1personel");
+    }
+    else if (ChunkCoordinate == "G/8"){
+        document.getElementById("Army1").innerHTML="阿科诺夫社会主义联邦";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="25k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "G/9"){
+        document.getElementById("Army1").innerHTML="阿科诺夫社会主义联邦";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="25k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "G/10"){
+        document.getElementById("Army1").innerHTML="阿科诺夫社会主义联邦";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="25k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "H/3"){
+        document.getElementById("Army1").innerHTML="羊村";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="50k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "H/5"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="40k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "H/6"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="40k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "H/7"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="40k";
+        display("army1personel");
+    }
+    
+    else if (ChunkCoordinate == "H/10"){
+        document.getElementById("Army1").innerHTML="阿科诺夫社会主义联邦";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="85k";
+        display("army1personel");
+    }
+    
+    else if (ChunkCoordinate == "I/5"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="50k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "I/6"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="50k";
+        display("army1personel");
+    }
+    
+    
+    
+    else if (ChunkCoordinate == "I/3"){
+        document.getElementById("Army1").innerHTML="羊村";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="50k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "J/3"){
+        document.getElementById("Army1").innerHTML="羊村";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="50k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "J/5"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "J/6"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "J/7"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "J/8"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }else if (ChunkCoordinate == "J/9"){
+        document.getElementById("Army1").innerHTML="冈比亚";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="60k";
+        display("army1personel");
+    }
+
+
+
+
+
+
+
+    else {
+        
+        document.getElementById("Army1").innerHTML="null";
+        display("army1");
+        document.getElementById("Army1personel").innerHTML="0k";
+        display("army1personel");
+        
+    }
+    
+}
